972bb88e167b2a254d63eda48f1544e6e4b71282
theantidoak <tcdoak@gmail.com>
2022-06-15 09:57:55 -0400

    Open pages in new tab, add profile pic, resize icons and elements

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const searchInput = document.querySelector("#searchbar");
 
 searchLabel.addEventListener('click', () => searchInput.style.display='block');
 searchInput.addEventListener('blur', () => searchInput.style.display='none');
+
+const darkLight = document.querySelector('#dark-light-mode');
+const sun = document.querySelector('#sun');
+const moon = document.querySelector('#moon');
+let celestialBodies = false;
+
+darkLight.addEventListener('click', () => {
+  if (!celestialBodies) {
+    sun.style.transform='translateY(5px)';
+    moon.style.transform='translateY(-5px)';
+  } else {
+    sun.style.transform='translateY(-5px)';
+    moon.style.transform='translateY(5px)';
+  }
+  celestialBodies = !celestialBodies;
+});
