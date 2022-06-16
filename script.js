a919386bdb63abf20195d9bd11d9e04428e83d08
theantidoak <tcdoak@gmail.com>
2022-06-16 14:20:09 -0400

    Finalize the highlight portion of the search results

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const suggestionDiv = document.querySelector('.suggestions');
 
 searchLabel.addEventListener('click', () => {
   searchInput.style.display='block';
-  // suggestionDiv.style.display='inline-block';
+  suggestionDiv.style.display='flex';
 });
 searchInput.addEventListener('blur', () => {
-  // searchInput.style.display='none';
-  // suggestionDiv.style.display='none';
+  searchInput.style.display='none';
+  suggestionDiv.style.display='none';
 });
 
 searchInput.addEventListener('input', findMatch);
@@ -40,15 +40,12 @@ function findMatch() {
                         .map((match) => match.index);
     
     const sug = document.createElement('div');
-    
     const result = document.createElement('a');
-    
-    const firstHalf = project.slice(0, reMatches[0]).toUpperCase();
-    const secondHalf = project.slice(reMatches[0] + 1).toUpperCase();
-
+    const firstHalf = project.slice(0, reMatches[0]);
+    const secondHalf = project.slice(reMatches[0] + this.value.length);
     const highlightSpan = document.createElement('span');
     highlightSpan.classList.add('project-match');
-    const highlightSpanContent = document.createTextNode(this.value.toUpperCase());
+    const highlightSpanContent = document.createTextNode(this.value);
     highlightSpan.appendChild(highlightSpanContent);
 
     const firstPart = document.createTextNode(firstHalf);
@@ -67,39 +64,75 @@ function findMatch() {
   });
   
 
-  // const upcomingMatches = upcomingProjects.filter((project) => project.match(regex));
-  // upcomingMatches.map((project) => {
-  //   const result = document.createElement('li');
-  //   const newResultContent = document.createTextNode(project);
-  //   const section = document.createElement('span');
-  //   const sectionContent = document.createTextNode('Future');
-  //   section.appendChild(sectionContent);
-  //   result.appendChild(newResultContent);
-  //   result.appendChild(section);
-  //   suggestionDiv.appendChild(result);
-  // });
+  const upcomingMatches = upcomingProjects.filter((project) => project.match(regex));
+  upcomingMatches.map((project) => {
+    const reMatches = [...project.matchAll(regex)]
+    .map((match) => match.index);
 
-  // const preTopMatches = preTopProjects.filter((project) => project.match(regex));
-  // preTopMatches.map((project) => {
-  //   const result = document.createElement('li');
-  //   const newResultContent = document.createTextNode(project);
-  //   const section = document.createElement('span');
-  //   const sectionContent = document.createTextNode('pre-TOP');
-  //   section.appendChild(sectionContent);
-  //   result.appendChild(newResultContent);
-  //   result.appendChild(section);
-  //   suggestionDiv.appendChild(result);
-  // });
+    const sug = document.createElement('div');
+    const result = document.createElement('a');
+    const firstHalf = project.slice(0, reMatches[0]);
+    const secondHalf = project.slice(reMatches[0] + this.value.length);
+    const highlightSpan = document.createElement('span');
+    highlightSpan.classList.add('project-match');
+    const highlightSpanContent = document.createTextNode(this.value);
+    highlightSpan.appendChild(highlightSpanContent);
+
+    const firstPart = document.createTextNode(firstHalf);
+    const secondPart = document.createTextNode(secondHalf);
+    result.appendChild(firstPart);
+    result.appendChild(highlightSpan);
+    result.appendChild(secondPart);
+
+    const section = document.createElement('span');
+    const sectionContent = document.createTextNode('Future');
+
+    section.appendChild(sectionContent);
+    sug.appendChild(result);
+    sug.appendChild(section);
+    suggestionDiv.appendChild(sug);
+    });
+
+  const preTopMatches = preTopProjects.filter((project) => project.match(regex));
+  preTopMatches.map((project) => {
+    const reMatches = [...project.matchAll(regex)]
+                        .map((match) => match.index);
+    
+    const sug = document.createElement('div');
+    const result = document.createElement('a');
+    const firstHalf = project.slice(0, reMatches[0]);
+    const secondHalf = project.slice(reMatches[0] + this.value.length);
+    const highlightSpan = document.createElement('span');
+    highlightSpan.classList.add('project-match');
+    const highlightSpanContent = document.createTextNode(this.value);
+    highlightSpan.appendChild(highlightSpanContent);
+
+    const firstPart = document.createTextNode(firstHalf);
+    const secondPart = document.createTextNode(secondHalf);
+    result.appendChild(firstPart);
+    result.appendChild(highlightSpan);
+    result.appendChild(secondPart);
+
+    const section = document.createElement('span');
+    const sectionContent = document.createTextNode('pre-TOP');
+
+    section.appendChild(sectionContent);
+    sug.appendChild(result);
+    sug.appendChild(section);
+    suggestionDiv.appendChild(sug);
+  });
   
   if (completedMatches == '' && upcomingMatches == '' && preTopMatches == '') {
-    const result = document.createElement('li');
+    const sug = document.createElement('div');
+    const result = document.createElement('a');
     const newResultContent = document.createTextNode('No results');
     result.appendChild(newResultContent);
-    suggestionDiv.appendChild(result);
+    sug.appendChild(result);
+    suggestionDiv.appendChild(sug);
   }
   if (this.value == '') {
     while(suggestionDiv.firstElementChild) {
-      suggestionDiv.removeChild(ul.lastChild);
+      suggestionDiv.removeChild(suggestionDiv.lastChild);
     }
   }
 }
